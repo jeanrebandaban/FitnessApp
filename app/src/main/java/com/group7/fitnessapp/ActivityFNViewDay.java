01c1ffeb89d59f742c74411f915c4d448107a980
jeanrebandaban <jeanrebandaban@gmail.com>
2022-03-06 22:17:35 +0800

    psush

---
+++ app/src/main/java/com/group7/fitnessapp/ActivityFNViewDay.java
@@ -0,0 +1,14 @@
+package com.group7.fitnessapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityFNViewDay extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fnviewday);
+    }
+}
